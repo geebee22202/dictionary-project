33018027e2cae6ec3cf4661f7c026c25720a9659
geebee22202 <90569659+geebee22202@users.noreply.github.com>
2022-05-04 10:50:28 -0400

    created dictionary form as a new component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary"
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="containter">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer"><small>Coded by Georgie</small></footer>
+      </div>
     </div>
   );
 }
-
-
