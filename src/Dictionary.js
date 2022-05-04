b2867ca5554727fb85fd9d6059d27156d49b495d
geebee22202 <90569659+geebee22202@users.noreply.github.com>
2022-05-04 18:56:47 -0400

    integrated api to display images

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,35 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos"
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
-  let [loaded, setLoaded] = useState(false)
+  let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null)
 
-  function handleResponse(response) {
-    console.log(response.data[0]);
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos)
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000010052abe3a9c14cef8bd9b226ec6bee2b";
+
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    axios
+      .get(pexelsApiUrl, {
+        headers: { Authorization: `Bearer ${pexelsApiKey}` },
+      })
+      .then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -28,30 +43,33 @@ export default function Dictionary(props) {
   }
 
   function load() {
-    setLoaded(true)
-    search()
+    setLoaded(true);
+    search();
   }
 
   if (loaded) {
-     return (
-    <div className="Dictionary">
-         <section>
-           <h1>What word do you want to look up?</h1>
-        <form onSubmit={handleSubmit}>
-          <input type="search" onChange={handleKeywordChange} defaultValue={props.defaultKeyword} />
-        </form>
-        <div className="suggested-words">
-          suggested words: pedantic, verbose,
-          supercalifragilisticexpialidocious...
-        </div>
-      </section>
-      <Results results={results} />
-    </div>
-  );
+    return (
+      <div className="Dictionary">
+        <section>
+          <h1>What word do you want to look up?</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
+          </form>
+          <div className="suggested-words">
+            suggested words: pedantic, verbose,
+            supercalifragilisticexpialidocious...
+          </div>
+        </section>
+        <Results results={results} />
+        <Photos photos={photos} />
+      </div>
+    );
   } else {
-    load()
-    return "Loading..."
+    load();
+    return "Loading...";
   }
-
- 
 }
